ac99ca71f595eef2ba0468d2f2a0aee258359cdf
cagina <a.cagin.taban@gmail.com>
2016-11-07 20:40:54 +0300

    added file

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Sitka Display", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(12, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start-NEXT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 151);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(636, 446);
+            this.listBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "23",
+            "100",
+            "500",
+            "1000"});
+            this.comboBox1.Location = new System.Drawing.Point(476, 107);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(138, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "Select Students";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(648, 151);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Max Match-up Day";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(544, 202);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(442, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(395, 29);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "                     BIRTHDAY PARADOX ";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Sitka Display", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(239, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 45);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "END";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(648, 359);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(544, 238);
+            this.listBox2.TabIndex = 7;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "January-March",
+            "April-June",
+            "July-September",
+            "October-December"});
+            this.comboBox2.Location = new System.Drawing.Point(476, 83);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(138, 21);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.Text = "Select Months";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1192, 596);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = " ";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+
+    }
+   
+}
+
---
+++ Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace WindowsFormsApplication4
+{    
+   
+    public partial class Form1 : Form
+    {
+        int max_kisi=0,sayaç_deneme=0,list_box_sayaci=0,KONTROL_SAYACİ=0;
+        int[,] ana_doğum_günleri = new int[12,31];
+        string[] ay_isimleri = { "January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+        int min_ay = 0;
+        int max_ay = 12;
+            
+         
+       
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+     
+              private void button1_Click(object sender, EventArgs e)
+        {
+            ++sayaç_deneme;
+            if (KONTROL_SAYACİ == 1)
+            max_ay = 3;
+            if (KONTROL_SAYACİ == 2)
+            {
+                min_ay = 3;
+                max_ay = 6;
+            }
+            if (KONTROL_SAYACİ == 3)
+            {
+                min_ay = 6;
+                max_ay = 9;
+            }
+            if (KONTROL_SAYACİ == 4)
+            {
+                min_ay = 9;
+                max_ay = 12;
+            }
+            int[][] dogumgünleri = new int[12][];
+            dogumgünleri[0] = new int[31];
+            dogumgünleri[1] = new int[29];
+            dogumgünleri[2] = new int[31];
+            dogumgünleri[3] = new int[30];
+            dogumgünleri[4] = new int[31];
+            dogumgünleri[5] = new int[30];
+            dogumgünleri[6] = new int[31];
+            dogumgünleri[7] = new int[31];
+            dogumgünleri[8] = new int[30];
+            dogumgünleri[9] = new int[31];
+            dogumgünleri[10] = new int[30];
+            dogumgünleri[11] = new int[31];
+            listBox1.Items.Add("  ");
+            listBox1.Items.Add("\t\t\t\t\t    " + sayaç_deneme + ". Experiment  ");
+            listBox1.Items.Add("  ");
+            Random aylar,günler; 
+            aylar  = new Random(DateTime.Now.Second-10000);
+            günler = new Random(DateTime.Now.Second+10000);
+         
+           for (int kisiler = 0; kisiler < max_kisi; kisiler++)
+            {
+
+                int ay = aylar.Next(min_ay,max_ay);
+                int gün = günler.Next((int)dogumgünleri[ay].Length) ;
+                ++dogumgünleri[ay][gün];
+                
+            }
+           listBox1.Items.Add("                  1  2  3  4  5  6  7  8  9  10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25  26  27  28  29  30  31");
+           listBox1.Items.Add("                  ---------------------------------------------------------------------------------------------------------------------------------------------------------------------- ");
+           int toplam_çakisma = 0, satir_sayaci = -1, gunler_sayaci = 0, aktarma_sayaci = -1;
+           for (int i = 0; i < 12;i++ )
+           {
+               gunler_sayaci=dogumgünleri[i].Length;
+               for (int j = 0; j < gunler_sayaci; j++)
+               {
+                   if (dogumgünleri[i][j] > 1)
+                       ++toplam_çakisma;
+               }
+                   if (gunler_sayaci == 31 && ay_isimleri[i].Length < 8)
+                       listBox1.Items.Add("" + ay_isimleri[i] + "\t   " + dogumgünleri[i][0] + "  " + dogumgünleri[i][1] + "  " + dogumgünleri[i][2] + "  " + dogumgünleri[i][3] + "  " + dogumgünleri[i][4] + "  " + dogumgünleri[i][5] + "  " + dogumgünleri[i][6] + "  " + dogumgünleri[i][7] + "  " + dogumgünleri[i][8] + "   " + dogumgünleri[i][9] + "    " + dogumgünleri[i][10] + "    " + dogumgünleri[i][11] + "    " + dogumgünleri[i][12] + "    " + dogumgünleri[i][13] + "    " + dogumgünleri[i][14] + "    " + dogumgünleri[i][15] + "    " + dogumgünleri[i][16] + "    " + dogumgünleri[i][17] + "    " + dogumgünleri[i][18] + "    " + dogumgünleri[i][19] + "    " + dogumgünleri[i][20] + "    " + dogumgünleri[i][21] + "    " + dogumgünleri[i][22] + "    " + dogumgünleri[i][23] + "    " + dogumgünleri[i][24] + "    " + dogumgünleri[i][25] + "    " + dogumgünleri[i][26] + "    " + dogumgünleri[i][27] + "    " + dogumgünleri[i][28] + "    " + dogumgünleri[i][29] + "    " + dogumgünleri[i][30]);
+                   else if (gunler_sayaci == 30 && ay_isimleri[i].Length < 8)
+                       listBox1.Items.Add("" + ay_isimleri[i] + "\t   " + dogumgünleri[i][0] + "  " + dogumgünleri[i][1] + "  " + dogumgünleri[i][2] + "  " + dogumgünleri[i][3] + "  " + dogumgünleri[i][4] + "  " + dogumgünleri[i][5] + "  " + dogumgünleri[i][6] + "  " + dogumgünleri[i][7] + "  " + dogumgünleri[i][8] + "   " + dogumgünleri[i][9] + "    " + dogumgünleri[i][10] + "    " + dogumgünleri[i][11] + "    " + dogumgünleri[i][12] + "    " + dogumgünleri[i][13] + "    " + dogumgünleri[i][14] + "    " + dogumgünleri[i][15] + "    " + dogumgünleri[i][16] + "    " + dogumgünleri[i][17] + "    " + dogumgünleri[i][18] + "    " + dogumgünleri[i][19] + "    " + dogumgünleri[i][20] + "    " + dogumgünleri[i][21] + "    " + dogumgünleri[i][22] + "    " + dogumgünleri[i][23] + "    " + dogumgünleri[i][24] + "    " + dogumgünleri[i][25] + "    " + dogumgünleri[i][26] + "    " + dogumgünleri[i][27] + "    " + dogumgünleri[i][28] + "    " + dogumgünleri[i][29]);
+               if(gunler_sayaci ==29 && ay_isimleri[i].Length<8)
+               listBox1.Items.Add("" + ay_isimleri[i] + "\t   " + dogumgünleri[i][0] + "  " + dogumgünleri[i][1] + "  " + dogumgünleri[i][2] + "  " + dogumgünleri[i][3] + "  " + dogumgünleri[i][4] + "  " + dogumgünleri[i][5] + "  " + dogumgünleri[i][6] + "  " + dogumgünleri[i][7] + "  " + dogumgünleri[i][8] + "   " + dogumgünleri[i][9] + "    " + dogumgünleri[i][10] + "    " + dogumgünleri[i][11] + "    " + dogumgünleri[i][12] + "    " + dogumgünleri[i][13] + "    " + dogumgünleri[i][14] + "    " + dogumgünleri[i][15] + "    " + dogumgünleri[i][16] + "    " + dogumgünleri[i][17] + "    " + dogumgünleri[i][18] + "    " + dogumgünleri[i][19] + "    " + dogumgünleri[i][20] + "    " + dogumgünleri[i][21] + "    " + dogumgünleri[i][22] + "    " + dogumgünleri[i][23] + "    " + dogumgünleri[i][24] + "    " + dogumgünleri[i][25] + "    " + dogumgünleri[i][26] + "    " + dogumgünleri[i][27] + "    " + dogumgünleri[i][28]);
+               else if(gunler_sayaci ==30 && ay_isimleri[i].Length>8)
+                   listBox1.Items.Add("" + ay_isimleri[i] + " " + dogumgünleri[i][0] + "  " + dogumgünleri[i][1] + "  " + dogumgünleri[i][2] + "  " + dogumgünleri[i][3] + "  " + dogumgünleri[i][4] + "  " + dogumgünleri[i][5] + "  " + dogumgünleri[i][6] + "  " + dogumgünleri[i][7] + "  " + dogumgünleri[i][8] + "   " + dogumgünleri[i][9] + "    " + dogumgünleri[i][10] + "    " + dogumgünleri[i][11] + "    " + dogumgünleri[i][12] + "    " + dogumgünleri[i][13] + "    " + dogumgünleri[i][14] + "    " + dogumgünleri[i][15] + "    " + dogumgünleri[i][16] + "    " + dogumgünleri[i][17] + "    " + dogumgünleri[i][18] + "    " + dogumgünleri[i][19] + "    " + dogumgünleri[i][20] + "    " + dogumgünleri[i][21] + "    " + dogumgünleri[i][22] + "    " + dogumgünleri[i][23] + "    " + dogumgünleri[i][24] + "    " + dogumgünleri[i][25] + "    " + dogumgünleri[i][26] + "    " + dogumgünleri[i][27] + "    " + dogumgünleri[i][28] + "    " + dogumgünleri[i][29]);
+               if(gunler_sayaci ==30 && ay_isimleri[i].Length==8)
+                   listBox1.Items.Add("" + ay_isimleri[i] + "  " + dogumgünleri[i][0] + "  " + dogumgünleri[i][1] + "  " + dogumgünleri[i][2] + "  " + dogumgünleri[i][3] + "  " + dogumgünleri[i][4] + "  " + dogumgünleri[i][5] + "  " + dogumgünleri[i][6] + "  " + dogumgünleri[i][7] + "  " + dogumgünleri[i][8] + "   " + dogumgünleri[i][9] + "    " + dogumgünleri[i][10] + "    " + dogumgünleri[i][11] + "    " + dogumgünleri[i][12] + "    " + dogumgünleri[i][13] + "    " + dogumgünleri[i][14] + "    " + dogumgünleri[i][15] + "    " + dogumgünleri[i][16] + "    " + dogumgünleri[i][17] + "    " + dogumgünleri[i][18] + "    " + dogumgünleri[i][19] + "    " + dogumgünleri[i][20] + "    " + dogumgünleri[i][21] + "    " + dogumgünleri[i][22] + "    " + dogumgünleri[i][23] + "    " + dogumgünleri[i][24] + "    " + dogumgünleri[i][25] + "    " + dogumgünleri[i][26] + "    " + dogumgünleri[i][27] + "    " + dogumgünleri[i][28] + "    " + dogumgünleri[i][29]);
+               if (gunler_sayaci == 31 && ay_isimleri[i].Length >= 8)
+                   listBox1.Items.Add("" + ay_isimleri[i] + "  " + dogumgünleri[i][0] + "  " + dogumgünleri[i][1] + "  " + dogumgünleri[i][2] + "  " + dogumgünleri[i][3] + "  " + dogumgünleri[i][4] + "  " + dogumgünleri[i][5] + "  " + dogumgünleri[i][6] + "  " + dogumgünleri[i][7] + "  " + dogumgünleri[i][8] + "   " + dogumgünleri[i][9] + "    " + dogumgünleri[i][10] + "    " + dogumgünleri[i][11] + "    " + dogumgünleri[i][12] + "    " + dogumgünleri[i][13] + "    " + dogumgünleri[i][14] + "    " + dogumgünleri[i][15] + "    " + dogumgünleri[i][16] + "    " + dogumgünleri[i][17] + "    " + dogumgünleri[i][18] + "    " + dogumgünleri[i][19] + "    " + dogumgünleri[i][20] + "    " + dogumgünleri[i][21] + "    " + dogumgünleri[i][22] + "    " + dogumgünleri[i][23] + "    " + dogumgünleri[i][24] + "    " + dogumgünleri[i][25] + "    " + dogumgünleri[i][26] + "    " + dogumgünleri[i][27] + "    " + dogumgünleri[i][28] + "    " + dogumgünleri[i][29] + "    " + dogumgünleri[i][30]);
+               
+           }
+           listBox1.Items.Add("");
+           listBox1.Items.Add("");
+           listBox1.Items.Add(" Total mappings = "+toplam_çakisma);      
+            satir_sayaci = -1; gunler_sayaci = -1; aktarma_sayaci = -1;
+            foreach (var satir in dogumgünleri)
+            {
+                ++satir_sayaci;
+                foreach (var gunler in satir)
+                {
+                    ++gunler_sayaci;
+                    ++aktarma_sayaci;
+                   
+                    if (gunler > 1)
+                    {
+                        ana_doğum_günleri[satir_sayaci,aktarma_sayaci] += gunler;
+                        listBox1.Items.Add("");
+                        listBox1.Items.Add("");
+                        listBox1.Items.Add("-->   " + (gunler_sayaci+1) + "   " + ay_isimleri[satir_sayaci] + "   " + (gunler-1) + " Match-up   <--");
+
+                    }
+                }
+                gunler_sayaci = -1;
+                aktarma_sayaci = -1;
+
+            }
+            int gecici = 0, ayın_yeri = 0, günün_yeri = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 31; j++)
+                {
+                    if (i == 0 && j == 0)
+                        gecici = ana_doğum_günleri[i, j];
+                    if (gecici < ana_doğum_günleri[i, j])
+                    {
+                        ayın_yeri = i;
+                        günün_yeri = j;
+                        gecici = ana_doğum_günleri[i, j];
+                    }
+
+
+                }
+            }
+            this.chart1.Series["Max Match-up Day"].Points.AddXY("" + ay_isimleri[ayın_yeri] + " " + (günün_yeri + 1), (gecici - 1));
+             
+
+        }
+
+              private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+              {
+                  if (comboBox1.SelectedIndex == 0)
+                  {
+                      if (max_kisi != 0)
+                      listBox1.Items.Clear();
+                      max_kisi = 23 ;
+                  }
+
+                  if (comboBox1.SelectedIndex == 1)
+                  {
+                      if (max_kisi != 0)
+                      listBox1.Items.Clear();
+                      max_kisi = 100;
+                  
+                  }
+
+                  if (comboBox1.SelectedIndex == 2)
+                  {
+                      if (max_kisi != 0)
+                      listBox1.Items.Clear();
+                      max_kisi = 500;
+                  }
+                  if (comboBox1.SelectedIndex == 3)
+                  {
+                      if (max_kisi != 0)
+                          listBox1.Items.Clear();
+                      max_kisi = 1000;
+                  } 
+              }
+
+              private void button2_Click(object sender, EventArgs e)
+              {
+                  ++list_box_sayaci;
+                  if (list_box_sayaci >1)
+                  listBox2.Items.Clear();
+                  listBox2.Items.Add("  ");
+                  listBox2.Items.Add("      Match-Up Days                      Average");
+                  listBox2.Items.Add("  --------------------------------           ------------------------------");
+                  for (int i = 0; i < 12; i++)
+                  {
+                      for (int j = 0; j < 31; j++)
+                      {
+                          if (ana_doğum_günleri[i, j] > 1 && ay_isimleri[i].Length<8)
+                              listBox2.Items.Add("            " + ay_isimleri[i] + " " + (j + 1) + "\t                         " + (double)(ana_doğum_günleri[i, j]-1) / sayaç_deneme);
+                          else if (ana_doğum_günleri[i, j] > 1 && ay_isimleri[i].Length >= 8)
+                              listBox2.Items.Add("            " + ay_isimleri[i] + " " + (j + 1) + "\t         " + (double)(ana_doğum_günleri[i, j] - 1) / sayaç_deneme);
+                        
+                         
+                         }
+                  }
+
+
+              }
+
+              private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+              {
+                  if (comboBox2.SelectedIndex == 0)
+                      KONTROL_SAYACİ = 1;
+                  if (comboBox2.SelectedIndex == 1)
+                      KONTROL_SAYACİ = 2;
+                  if (comboBox2.SelectedIndex == 2)
+                      KONTROL_SAYACİ = 3;
+                  if (comboBox2.SelectedIndex == 3)
+                      KONTROL_SAYACİ = 4;
+              }
+
+              
+              
+
+             
+
+            
+
+              
+
+             
+
+             
+       
+    }
+}
